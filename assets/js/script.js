323698ec629144c2ea2d6b1265c1769fbd4af80e
Jollyrgr83 <jasongmangin@gmail.com>
2020-03-27 19:01:17 -0600

    added education section and fixed some layout issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var aboutMeButtonEl = document.getElementById("about-me-link");
 var portfolioButtonEl = document.getElementById("portfolio-link");
 var contactMeButtonEl = document.getElementById("contact-link");
-
-var aboutMePageEl = document.getElementById("about-me-page");
+var aboutMePageEl0 = document.getElementById("about-me-page-0");
+var aboutMePageEl1 = document.getElementById("about-me-page-1");
 var portfolioPageEl0 = document.getElementById("portfolio-page-0");
 var portfolioPageEl1 = document.getElementById("portfolio-page-1");
 var portfolioPageEl2 = document.getElementById("portfolio-page-2");
@@ -10,7 +10,8 @@ var contactMePageEl = document.getElementById("contact-me-page");
 
 aboutMeButtonEl.addEventListener("click", function(event) {
     event.preventDefault();
-    aboutMePageEl.setAttribute("class", "container main-container p-3 m-3 mx-auto show");            
+    aboutMePageEl0.setAttribute("class", "container main-container p-3 m-3 mx-auto show");
+    aboutMePageEl1.setAttribute("class", "container main-container p-3 m-3 mx-auto show");             
     portfolioPageEl0.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");
     portfolioPageEl1.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");
     portfolioPageEl2.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");
@@ -22,14 +23,16 @@ portfolioButtonEl.addEventListener("click", function(event) {
     portfolioPageEl0.setAttribute("class", "container main-container p-3 m-3 mx-auto show");
     portfolioPageEl1.setAttribute("class", "container main-container p-3 m-3 mx-auto show");
     portfolioPageEl2.setAttribute("class", "container main-container p-3 m-3 mx-auto show");
-    aboutMePageEl.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");            
+    aboutMePageEl0.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");
+    aboutMePageEl1.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");            
     contactMePageEl.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");
 });
 
 contactMeButtonEl.addEventListener("click", function(event) {
     event.preventDefault();
     contactMePageEl.setAttribute("class", "container main-container p-3 m-3 mx-auto show");
-    aboutMePageEl.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");            
+    aboutMePageEl0.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");            
+    aboutMePageEl1.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");
     portfolioPageEl0.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");
     portfolioPageEl1.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");
     portfolioPageEl2.setAttribute("class", "container main-container p-3 m-3 mx-auto hide");
