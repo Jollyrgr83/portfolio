90e4f4c966fad2e271f38e35180dff0e2b31c17e
Jollyrgr83 <jasongmangin@gmail.com>
2020-04-21 15:36:11 -0600

    updated formatting on about me page

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,49 +9,49 @@ var portfolio = {
         },
         1 : {
             "title" : "game",
-            "url" : "https://jollygrg83.github.io/game/",
+            "url" : "https://jollyrgr83.github.io/game/",
             "image" : "./assets/images/game.png",
             "alt" : "screenshot of game home screen",
             "text" : "This is the beginning of a war-based role playing game. It contains dynamic html updating and DOM manipulation using javascript. It is a work in progress."
         },
         2 : {
             "title" : "password generator",
-            "url" : "https://jollygrg83.github.io/password-generator/",
+            "url" : "https://jollyrgr83.github.io/password-generator/",
             "image" : "./assets/images/password-generator.png",
             "alt" : "screenshot of password generator home screen",
             "text" : "This is a password generator that allows you to select criteria and randomly generate a password meeting those criteria. Specifically, you can select a password from 8 to 128 (inclusive) characters consisting of lowercase, uppercase, numeric, and/or special characters."
         },
         3 : {
             "title" : "password vault",
-            "url" : "https://jollygrg83.github.io/password-vault/",
+            "url" : "https://jollyrgr83.github.io/password-vault/",
             "image" : "./assets/images/password-vault.png",
             "alt" : "screenshot of password vault home screen",
             "text" : "This is a quick project that is in its beginning phases. It is intended to be a mobile site that stores and displays login information for various websites."
         },
         4 : {
             "title" : "event scoring database",
-            "url" : "https://jollygrg83.github.io/event-scoring-database/",
+            "url" : "https://jollyrgr83.github.io/event-scoring-database/",
             "image" : "./assets/images/event-scoring-database.png",
             "alt" : "screenshot of event scoring database home screen",
             "text" : "This is the beginning of a larger project that I hope to have completed later this year. A public power utility in the southeast organizes a lineworkers rodeo event every year that consists of two tiers of competitors, each completing five events that receive an associated score and time. Calculating and reporting the overall scores has historically been a challenge due to the amount and type of data and the need for tie-breaking. At completion, this project will record, analyze, and display data as well as generate the necessary reports for this event."
         },
         5 : {
             "title" : "day planner",
-            "url" : "https://jollygrg83.github.io/day-planner/",
+            "url" : "https://jollyrgr83.github.io/day-planner/",
             "image" : "./assets/images/day-planner.png",
             "alt" : "screenshot of day planner home screen",
             "text" : "This is a day planner project that was assigned as part of the University of Utah full stack coding bootcamp. It dynamically updates formatting based on the current hour and allows users to input tasks for a workday."
         },
         6 : {
             "title" : "Skyvi3w",
-            "url" : "https://jollygrg83.github.io/Project-1/",
+            "url" : "https://jollyrgr83.github.io/Project-1/",
             "image" : "./assets/images/skyvi3w.PNG",
             "alt" : "screenshot of skyvi3w home screen",
             "text" : "Skyvi3w is a search engine that delivers information about upcoming astral, earth, and weather events from NASA and OpenWeather APIs against the stunning visuals provided by NASA's Astronomy Picture of the Day."
         },
         7 : {
             "title" : "weather dashboard",
-            "url" : "https://jollygrg83.github.io/weather-dashboard/",
+            "url" : "https://jollyrgr83.github.io/weather-dashboard/",
             "image" : "./assets/images/weather-dashboard.png",
             "alt" : "screenshot of weather dashboard home screen",
             "text" : "This is a weather dashboard project that was assigned as part of the University of Utah full stack coding bootcamp. It dynamically updates a weather display including images, temperature, humidity, uv index, windspeed, and a five day forecast using the openweathermap API."
