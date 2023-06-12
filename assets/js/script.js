278be8e9d1bf4d1d8f47d1845af9a022a5f9f19f
Jason Mangin <jmangin@goldrushexpeditions.com>
2023-06-12 13:30:12 -0600

    added content and functionality for professional projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,75 @@
 class Portfolio {
+  professional;
   projects;
   keys;
-  containers;
+  containerEls;
+  projectEls;
+  professionalEls;
+  buttonEls;
+  selectEls;
   pageState;
   constructor() {
     this.setContent();
     this.getEls();
     this.setEvents();
     this.pageState = ["show", "hide", "hide"];
-    this.renderProjectMenu();
+    this.renderMenus();
+    this.displayProfessional(0);
     this.displayProject(0);
     this.displayPage("about");
   }
   setContent() {
-    this.projects = {
+    this.professional = {
       0: {
-        title: "Bier Run",
-        url: "https://bier-run.herokuapp.com/",
-        git: "https://github.com/Jollyrgr83/bierRun",
-        image: "./assets/images/project-bier-run.png",
-        alt: "screenshot of Bier run home screen",
+        title: "Gold Rush Expeditions, Inc®",
+        url: "https://goldrushexpeditions.com",
+        git: "false",
+        image: "./assets/images/professional-gre.png",
+        alt: "screenshot of Gold Rush Expeditions, Inc® home screen",
         text:
-            "Bier Run was developed as the final project of the UofU Coding Bootcamp. It is a MERN stack allowing customers to place beer delivery orders and vendors to fulfill them. Bier Run utilizes Auth0 for registration and login.",
+            "GRE text",
       },
       1: {
+        title: "National Bureau of Mines®",
+        url: "https://nationalbureauofmines.com",
+        git: "false",
+        image: "./assets/images/professional-nbom.png",
+        alt: "screenshot of National Bureau of Mines® home screen",
+        text:
+            "NBOM text",
+      },
+      2: {
+        title: "Stay Out Stay Alive®",
+        url: "https://stayoutstayalive.com",
+        git: "false",
+        image: "./assets/images/professional-sosa.png",
+        alt: "screenshot of Stay Out Stay Alive® home screen",
+        text:
+            "SOSA text",
+      },
+    };
+    this.projects = {
+      0: {
         title: "Rodeo Scoring",
-        url: "https://rodeo-scoring-version-1.herokuapp.com/",
+        url: "false",
         git: "https://github.com/Jollyrgr83/rodeo-scoring-version-1",
         image: "./assets/images/project-rodeo-scoring-version-1.png",
         alt: "screenshot of Rodeo Scoring home screen",
         text:
             "Rodeo Scoring was developed to assist a public power utility (or similar entity) in facilitating an annual lineworkers rodeo (or similar competition). Rodeo Scoring uses jQuery, Bootstrap, MySQL, Node.js, and Express to allow users to enter competitor, event, competition tier, and score info and generate necessary reports for standings and results.",
       },
+      1: {
+        title: "Bier Run",
+        url: "false",
+        git: "https://github.com/Jollyrgr83/bierRun",
+        image: "./assets/images/project-bier-run.png",
+        alt: "screenshot of Bier run home screen",
+        text:
+            "Bier Run was developed as the final project of the UofU Coding Bootcamp. It is a MERN stack allowing customers to place beer delivery orders and vendors to fulfill them. Bier Run utilizes Auth0 for registration and login.",
+      },
       2: {
         title: "Story Lines",
-        url: "https://uofu-project-2-story-lines.herokuapp.com/",
+        url: "false",
         git: "https://github.com/Jollyrgr83/uofu-project-story-lines",
         image: "./assets/images/project-story-lines.png",
         alt: "screenshot of Story Lines home screen",
@@ -42,7 +77,7 @@ class Portfolio {
       },
       3: {
         title: "Google Books Companion",
-        url: "https://uofu-homework-books.herokuapp.com/",
+        url: "false",
         git: "https://github.com/Jollyrgr83/uofu-hw-book-search",
         image: "./assets/images/project-books.png",
         alt: "screenshot of Google Books Companion home screen",
@@ -50,7 +85,7 @@ class Portfolio {
       },
       4: {
         title: "Employee Directory",
-        url: "https://jollyrgr83.github.io/uofu-hw-directory/",
+        url: "false",
         git: "https://github.com/Jollyrgr83/uofu-hw-directory",
         image: "./assets/images/project-directory.png",
         alt: "screenshot of Directory home screen",
@@ -58,7 +93,7 @@ class Portfolio {
       },
       5: {
         title: "Budget Tracker",
-        url: "https://uofu-homework-budget.herokuapp.com/",
+        url: "false",
         git: "https://github.com/Jollyrgr83/uofu-hw-budget-tracker",
         image: "./assets/images/project-budget.png",
         alt: "screenshot of Budget Tracker home screen",
@@ -66,7 +101,7 @@ class Portfolio {
       },
       6: {
         title: "Fitness Tracker",
-        url: "https://uofu-homework-fitness.herokuapp.com/",
+        url: "false",
         git: "https://github.com/Jollyrgr83/uofu-hw-fitness-tracker",
         image: "./assets/images/project-fitness.png",
         alt: "screenshot of Fitness Tracker home screen",
@@ -74,7 +109,7 @@ class Portfolio {
       },
       7: {
         title: "Burger Database",
-        url: "https://uofu-homework-burger.herokuapp.com/",
+        url: "false",
         git: "https://github.com/Jollyrgr83/uofu-hw-burger-database",
         image: "./assets/images/project-burger.png",
         alt: "screenshot of Burger Database home screen",
@@ -89,7 +124,10 @@ class Portfolio {
         text: "Use the links to view the codebase for this portfolio."
       }
     };
-    this.keys = Object.keys(this.projects);
+    this.keys = {
+      "projects": Object.keys(this.projects),
+      "professional": Object.keys(this.professional)
+    };
   }
   getEls() {
     this.containerEls = {
@@ -100,9 +138,19 @@ class Portfolio {
     this.projectEls = {
       "container": document.getElementById("project-container"),
       "image": document.getElementById("project-image"),
+      "live_link_container": document.getElementById("project-live-link-container"),
       "live_link": document.getElementById("project-live-link"),
       "repo_link": document.getElementById("project-repo-link"),
       "text": document.getElementById("project-text")
+    };
+    this.professionalEls = {
+      "container": document.getElementById("professional-container"),
+      "image": document.getElementById("professional-image"),
+      "live_link_container": document.getElementById("professional-live-link-container"),
+      "live_link": document.getElementById("professional-live-link"),
+      "repo_link_container": document.getElementById("professional-repo-link-container"),
+      "repo_link": document.getElementById("professional-repo-link"),
+      "text": document.getElementById("professional-text")
     };
     this.buttonEls = {
       "about": document.getElementById("about-btn"),
@@ -111,13 +159,17 @@ class Portfolio {
       "form": document.getElementById("form-btn")
     };
     this.selectEls = {
-      "project": document.getElementById("project-select")
+      "project": document.getElementById("project-select"),
+      "professional": document.getElementById("professional-select")
     };
   }
   setEvents() {
     this.selectEls.project.addEventListener("change", () => {
       this.displayProject(parseInt(this.selectEls.project.value));
     });
+    this.selectEls.professional.addEventListener("change", () => {
+      this.displayProfessional(parseInt(this.selectEls.professional.value));
+    })
     this.buttonEls.about.addEventListener("click", this.nav.bind(this));
     this.buttonEls.projects.addEventListener("click", this.nav.bind(this));
     this.buttonEls.contact.addEventListener("click", this.nav.bind(this));
@@ -150,20 +202,53 @@ class Portfolio {
     this.projectEls.container.setAttribute("class", "show");
     this.projectEls.image.setAttribute("src", this.projects[i].image);
     this.projectEls.image.setAttribute("alt", this.projects[i].alt);
-    this.projectEls.live_link.setAttribute("href", this.projects[i].url);
-    this.projectEls.live_link.setAttribute("target", "_blank");
-    this.projectEls.live_link.innerText = this.projects[i].title + " live page";
+    if (this.projects[i].url === "false") {
+      this.projectEls.live_link_container.setAttribute("style", "display: none;");
+    } else {
+      this.projectEls.live_link_container.setAttribute("style", "display: block;");
+      this.projectEls.live_link.setAttribute("href", this.projects[i].url);
+      this.projectEls.live_link.setAttribute("target", "_blank");
+      this.projectEls.live_link.innerText = this.projects[i].title + " live page";
+    }
     this.projectEls.repo_link.setAttribute("href", this.projects[i].git);
     this.projectEls.repo_link.setAttribute("target", "_blank");
     this.projectEls.repo_link.innerText = this.projects[i].title + " repository";
     this.projectEls.text.innerText = this.projects[i].text;
   }
-  renderProjectMenu() {
-    this.keys.forEach(k => {
+  displayProfessional(i) {
+    this.professionalEls.container.setAttribute("class", "show");
+    this.professionalEls.image.setAttribute("src", this.professional[i].image);
+    this.professionalEls.image.setAttribute("alt", this.professional[i].alt);
+    if (this.professional[i].url === "false") {
+      this.professionalEls.live_link_container.setAttribute("style", "display: none;");
+    } else {
+      this.professionalEls.live_link_container.setAttribute("style", "display: block;");
+      this.professionalEls.live_link.setAttribute("href", this.professional[i].url);
+      this.professionalEls.live_link.setAttribute("target", "_blank");
+      this.professionalEls.live_link.innerText = this.professional[i].title + " live page";
+    }
+    if (this.professional[i].git === "false") {
+      this.professionalEls.repo_link_container.setAttribute("style", "display: none;");
+    } else {
+      this.professionalEls.repo_link_container.setAttribute("style", "display: block;");
+      this.professionalEls.repo_link.setAttribute("href", this.professional[i].git);
+      this.professionalEls.repo_link.setAttribute("target", "_blank");
+      this.professionalEls.repo_link.innerText = this.professional[i].title + " repository";
+    }
+    this.professionalEls.text.innerText = this.professional[i].text;
+  }
+  renderMenus() {
+    this.keys.projects.forEach(k => {
       const el = document.createElement("option");
       el.value = k;
       el.innerText = this.projects[k].title;
       this.selectEls.project.appendChild(el);
+    });
+    this.keys.professional.forEach(k => {
+      const el = document.createElement("option");
+      el.value = k;
+      el.innerText = this.professional[k].title;
+      this.selectEls.professional.appendChild(el);
     });
   }
 }
